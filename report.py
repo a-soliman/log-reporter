fa1bfbc9d628f00acd91d87642725b7c75e182e3
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-05-19 14:13:38 -0700

    solved the 3rd problem and coded a printer method

--- report.py
+++ report.py
@@ -1,37 +1,6 @@
 import psycopg2
-import datetime
 
 DBNAME = "news"
-
-def get_slugs():
-    db      = psycopg2.connect(database=DBNAME)
-    cursor  = db.cursor()
-    query   = "SELECT slug FROM articles"
-    cursor.execute(query)
-    slugs = [slug[0] for slug in cursor.fetchall()]
-    db.close()
-    return slugs
-
-def get_authors():
-    db      = psycopg2.connect(database=DBNAME)
-    cursor  = db.cursor()
-    query   = "SELECT name FROM authors"
-    cursor.execute(query)
-    authors = [author[0] for author in cursor.fetchall()]
-    db.close()
-    return authors
-
-
-def test_log():
-    db      = psycopg2.connect(database=DBNAME)
-    cursor  = db.cursor()
-    query   = "SELECT * FROM log"
-    cursor.execute(query)
-    result  = [log[0] for log in cursor.fetchall()]
-    db.close()
-    return result
-
-# print(get_slugs())
 
 def get_most_popular_articles():
     db      = psycopg2.connect(database=DBNAME)
@@ -45,11 +14,9 @@ def get_most_popular_articles():
         ORDER BY views DESC LIMIT 3;
         """
     cursor.execute(query)
-    result  = [{"title": article[0], "views": int(article[1])} for article in cursor.fetchall()]
+    result  = [ (article[0], int(article[1])) for article in cursor.fetchall()]
     db.close()
     return result
-
-print(get_most_popular_articles())
 
 def get_most_popular_authors():
     db      = psycopg2.connect(database=DBNAME)
@@ -66,8 +33,62 @@ def get_most_popular_authors():
         ORDER BY views DESC;
         """
     cursor.execute(query)
-    result = [{"name": author[0], "views": int(author[1])} for author in cursor.fetchall()]
+    result = [(author[0], int(author[1])) for author in cursor.fetchall()]
     db.close()
     return result
 
-print(get_most_popular_authors())
+def get_error_per_day():
+    db      = psycopg2.connect(database=DBNAME)
+    cursor  = db.cursor()
+    query   = """SELECT 
+        day, ((err + .0) / (total+ .0)) * 100 AS per 
+        FROM (
+            SELECT 
+                date(time) AS day, 
+                COUNT(time) AS total, 
+                SUM(CASE WHEN status ='200 OK' THEN 1 ELSE 0 END) as ok, 
+                SUM(CASE WHEN status = '404 NOT FOUND' THEN 1 ELSE 0 END) AS err 
+            FROM log 
+            GROUP BY day) 
+            AS tab 
+            WHERE ((err+.0) / (total+.0)) *100 > 1 
+            ORDER BY per DESC;
+        """
+    cursor.execute(query)
+    result  = [(str(date[0]), round(float(date[1]), 2)) for date in cursor.fetchall()]
+    db.close()
+    return result
+
+#print(get_error_per_day())
+
+def report():
+    print('='*20 + ' REPORT START ' + '='*20)
+    printer('articles', get_most_popular_articles())
+    printer('authors', get_most_popular_authors())
+    printer('error', get_error_per_day())
+    print('='*20 + ' REPORT END ' + '='*20)
+    
+def printer(_type, data_array):
+    title   = ''
+    dash    = ' :==> '
+    ending  = ''
+
+    if _type == 'articles':
+        title   = 'What are the most popular three articles of all time?'
+        ending  = ' views.'
+
+    elif _type == 'authors':
+        title   = 'Who are the most popular article authors of all time?'
+        ending  = ' views.'
+    
+    else:
+        title   = 'On which days did more than 1% of requests lead to errors?'
+        ending  = '% errors.'
+    
+    print('--'*10 +'\n\n** ' + title + '\n\n')
+
+    for line in data_array:
+        print(line[0] + dash + str(line[1]) + ending + '\n')
+    print('--'*10)
+
+report()
